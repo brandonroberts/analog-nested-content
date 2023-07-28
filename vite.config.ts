70da8fdeb6f37178c99d73f429296757c518c5e7
Brandon Roberts <robertsbt@gmail.com>
2023-07-28 11:26:19 -0500

    feat: add vite-plugin-static-copy for post assets

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 
 import { defineConfig } from 'vite';
 import analog from '@analogjs/platform';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,11 +13,22 @@ export default defineConfig(({ mode }) => ({
   resolve: {
     mainFields: ['module'],
   },
-  plugins: [analog({
-    prerender: {
-      routes: ['/blog/foo-article']
-    }
-  })],
+  plugins: [
+    analog({
+      prerender: {
+        routes: ['/blog/foo-article']
+      }
+    }),
+    viteStaticCopy({
+      structured: true,
+      targets: [
+        {
+          src: 'src/content/posts/**/*.png',
+          dest: ''
+        }
+      ]
+    })    
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
